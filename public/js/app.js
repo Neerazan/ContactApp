5d520a226da9cbef55d692eecb1bf98f48946045
Neerajan Dhakal <nirajandhakal634@gmail.com>
2023-05-04 16:13:12 +0545

    validation added

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,9 @@
 document.getElementById('filter_company_id').addEventListener('change', function (){
+
     let companyId = this.value || this.option[this.selectedIndex].value
+
     window.location.href = window.location.href.split('?')[0] + '?company_id=' + companyId
+
+    //http://127.0.0.1:8000/contacts?page=4
+    //http://127.0.0.1:8000/contacts?company_id=4
 })
